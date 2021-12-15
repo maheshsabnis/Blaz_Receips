b336aca919a31ac443e2fb8b80e99b0152157acb
maheshsabnis <sabnis_m@hotmail.com>
2021-12-15 16:01:00 +0530

    Add project files.

---
+++ Blaz_CascadeDropdown/Models/Pagination.cs
@@ -0,0 +1,27 @@
+﻿namespace Blaz_CascadeDropdown.Models
+{
+    public class Pagination
+    {
+
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount(int recCount, int recPerPage)
+        {
+            return Convert.ToInt32(Math.Ceiling(recCount / (double)recPerPage));
+        }
+    }
+
+    
+
+
+    public class Paging
+    {
+        public string LinkText { get; set; }
+        public int PageId { get; set; }
+        
+        public Paging(int page, string text)
+        {
+            PageId = page;
+            LinkText = text;
+        }
+    }
+}
