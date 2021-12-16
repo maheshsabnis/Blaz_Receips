f4f85ea8e004c5da78e74aa9c2abf8819a11cbfe
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-12-16 17:49:57 +0530

    Update ProductCatalogContext.cs

--- APIService/Models/ProductCatalogContext.cs
+++ APIService/Models/ProductCatalogContext.cs
@@ -20,11 +20,7 @@ namespace APIService.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=tcp:msit.database.windows.net,1433;Initial Catalog=ProductCatalog;Persist Security Info=False;User ID=MaheshAdmin;Password=P@ssw0rd_;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-//            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
